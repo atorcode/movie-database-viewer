42bc2064b67bd35f9194338df2de43c81a0a1aae
atorcode <atorcode@gmail.com>
2021-11-30 20:54:53 -0800

    Add basic footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,21 @@
 import styles from "./Footer.module.scss";
+import TMDBShoutOut from "../../images/PoweredByTMDB.svg";
+
 const Footer = () => {
-  return <footer className={styles.a}>red</footer>;
+  return (
+    <footer className={styles["footer"]}>
+      <a href="#">
+        <img
+          src={TMDBShoutOut}
+          alt="Powered by TMDB"
+          className={styles["tmdb-shout-out"]}
+        />
+      </a>
+      <p>
+        This application consumes the TMDB API but is not affiliated with TMDB
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
