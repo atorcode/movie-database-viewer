4ff24ef8877bc724bfd4e3e407951cc9c6c546d6
atorcode <atorcode@gmail.com>
2021-11-30 21:31:10 -0800

    Add styling to search bar

--- src/components/_basic/SearchBar/SearchBar.js
+++ src/components/_basic/SearchBar/SearchBar.js
@@ -4,10 +4,10 @@ import { FiSearch } from "react-icons/fi";
 const SearchBar = () => {
   return (
     <div className={styles["search-bar-container"]}>
-      <FiSearch />
+      <FiSearch className={styles["search-bar-icon"]} />
       <input
         type="search"
-        placeholder="Search..."
+        placeholder="Search for a movie..."
         className={styles["search-bar"]}
       />
     </div>
