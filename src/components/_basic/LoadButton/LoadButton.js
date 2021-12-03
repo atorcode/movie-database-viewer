63c9aec7c43364e5f1c659f6272a89f513156962
atorcode <atorcode@gmail.com>
2021-12-02 16:18:18 -0800

    Add dynamic fetching with load button

--- src/components/_basic/LoadButton/LoadButton.js
+++ src/components/_basic/LoadButton/LoadButton.js
@@ -1,12 +1,29 @@
+import { useState, useEffect } from "react";
 import styles from "./LoadButton.module.scss";
-
+import useSWR from "swr";
+import fetcher from "../../../helpers/fetcher";
 const LoadButton = ({ displayedMovies, setDisplayedMovies }) => {
+  const [startFetch, setStartFetch] = useState(false);
+  const { data, error } = useSWR(() => {
+    return startFetch
+      ? `https://api.themoviedb.org/3/trending/movie/week?api_key=${process.env.REACT_APP_API_KEY}&page=2`
+      : null;
+  }, fetcher);
+
+  useEffect(() => {
+    if (data) {
+      setDisplayedMovies(data.results);
+    }
+  }, [data]);
+
   return (
     <>
       <button
         type="button"
         className={styles["load-more-button"]}
-        onClick={() => {}}
+        onClick={() => {
+          setStartFetch(true);
+        }}
       >
         Load more
       </button>
