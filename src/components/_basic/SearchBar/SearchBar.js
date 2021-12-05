668588f6c6a8431b8730236057f20f4ffaac2334
atorcode <atorcode@gmail.com>
2021-12-05 01:00:14 -0800

    Improve search bar styling and make icon focus field

--- src/components/_basic/SearchBar/SearchBar.js
+++ src/components/_basic/SearchBar/SearchBar.js
@@ -1,11 +1,19 @@
+import { useRef } from "react";
 import styles from "./SearchBar.module.scss";
 import { FiSearch } from "react-icons/fi";
 
 const SearchBar = () => {
+  const inputEl = useRef(null);
   return (
     <div className={styles["search-bar-container"]}>
-      <FiSearch className={styles["search-bar-icon"]} />
+      <FiSearch
+        className={styles["search-bar-icon"]}
+        onClick={() => {
+          inputEl.current.focus();
+        }}
+      />
       <input
+        ref={inputEl}
         type="search"
         placeholder="Search for a movie..."
         className={styles["search-bar"]}
