01d38d22d80f57041c19474c9174e670b8921cdc
atorcode <atorcode@gmail.com>
2021-12-11 01:12:56 -0800

    Properly situate top of SearchResults panel behind search bar

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,7 +1,7 @@
 import styles from "./SearchResults.module.scss";
 
 const SearchResults = () => {
-  return <div className={styles["search-results"]}>WASSUP</div>;
+  return <div className={styles["search-results"]}></div>;
 };
 
 export default SearchResults;
