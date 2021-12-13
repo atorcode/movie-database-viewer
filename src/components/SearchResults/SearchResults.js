ca630974e6854edb4b651668e95e35e1ddf9910e
atorcode <atorcode@gmail.com>
2021-12-12 21:03:58 -0800

    Make search results panel responsive and limit shown results to 10

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -3,15 +3,23 @@ import styles from "./SearchResults.module.scss";
 import SearchResult from "../SearchResult";
 
 const SearchResults = ({ data }) => {
+  const testArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  console.log(testArr);
+
   return (
     <div className={styles["search-results-panel"]}>
-      {data && (
+      {/* {data && (
         <ul className={styles["search-results"]}>
           {data.results.map((result) => {
             return <SearchResult />;
           })}
         </ul>
-      )}
+      )} */}
+      <ul className={styles["search-results"]}>
+        {testArr.map((result) => {
+          return <SearchResult />;
+        })}
+      </ul>
       {/* {!data && <p>Loading</p>} */}
 
       {data && !data.total_results && (
