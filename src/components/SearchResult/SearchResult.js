07edacfaf3726a283458726fb8d9672cbb695887
atorcode <atorcode@gmail.com>
2021-12-12 23:57:10 -0800

    Make each search result link to the appropriate page

--- src/components/SearchResult/SearchResult.js
+++ src/components/SearchResult/SearchResult.js
@@ -1,7 +1,14 @@
 import styles from "./SearchResult.module.scss";
+import { Link } from "react-router-dom";
 
-const SearchResult = () => {
-  return <li className={styles["search-result"]}>WEEEEEEEEEEEEE</li>;
+const SearchResult = ({ id, title }) => {
+  return (
+    <li className={styles["search-result"]}>
+      <Link to={`/movie/${id}`} className={styles["search-link"]}>
+        {title}
+      </Link>
+    </li>
+  );
 };
 
 export default SearchResult;
--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -3,23 +3,22 @@ import styles from "./SearchResults.module.scss";
 import SearchResult from "../SearchResult";
 
 const SearchResults = ({ data }) => {
-  const testArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-  console.log(testArr);
+  let results;
+
+  if (data) {
+    ({ results } = data);
+  }
 
   return (
     <div className={styles["search-results-panel"]}>
-      {/* {data && (
+      {results && (
         <ul className={styles["search-results"]}>
-          {data.results.map((result) => {
-            return <SearchResult />;
+          {results.map((result) => {
+            return <SearchResult key={result.id} {...result} />;
           })}
         </ul>
-      )} */}
-      <ul className={styles["search-results"]}>
-        {testArr.map((result) => {
-          return <SearchResult />;
-        })}
-      </ul>
+      )}
+
       {/* {!data && <p>Loading</p>} */}
 
       {data && !data.total_results && (
