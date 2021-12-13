32d83acfb007f19a1d385532c11c82b53a90fa4d
atorcode <atorcode@gmail.com>
2021-12-13 15:43:08 -0800

    Fix search bar blur bug and window scrolling

--- src/components/SearchResult/SearchResult.js
+++ src/components/SearchResult/SearchResult.js
@@ -4,7 +4,18 @@ import { Link } from "react-router-dom";
 const SearchResult = ({ id, title }) => {
   return (
     <li className={styles["search-result"]}>
-      <Link to={`/movie/${id}`} className={styles["search-link"]}>
+      <Link
+        to={`/movie/${id}`}
+        tabIndex={-1}
+        onMouseDown={(e) => {
+          e.preventDefault();
+        }}
+        onClick={() => {
+          document.activeElement.blur();
+          window.scrollTo(0, 0);
+        }}
+        className={styles["search-link"]}
+      >
         {title}
       </Link>
     </li>
--- src/pages/SelectedMovie/SelectedMovie.js
+++ src/pages/SelectedMovie/SelectedMovie.js
@@ -5,6 +5,7 @@ import MovieDetails from "../../components/MovieDetails";
 import Cast from "../../components/Cast";
 
 const SelectedMovie = () => {
+  // This useEffect will not trigger when switching from a movie page to another movie page, since only the data changes and there is no mounting/unmounting going on.
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
