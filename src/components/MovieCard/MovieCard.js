f78913f30726994f041e585d4c030bb5181decf7
atorcode <atorcode@gmail.com>
2021-12-20 02:16:53 -0800

    Fix one-column layout and prevent favoriting from focusing card

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -59,6 +59,8 @@ const MovieCard = (props) => {
           <path
             d="M12 4.419c-2.826-5.695-11.999-4.064-11.999 3.27 0 7.27 9.903 10.938 11.999 15.311 2.096-4.373 12-8.041 12-15.311 0-7.327-9.17-8.972-12-3.27z"
             ref={pathEl}
+            // prevent card from becoming focused
+            onMouseDown={(e) => e.preventDefault()}
             onClick={(e) => {
               // stop click from registering on the movie card
               e.preventDefault();
