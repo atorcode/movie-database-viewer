4185381b881610f18960149609ad08a379bda940
atorcode <atorcode@gmail.com>
2021-12-20 18:11:23 -0800

    Dynamically set justify-content override of movies-container

--- src/components/MovieCards/MovieCards.js
+++ src/components/MovieCards/MovieCards.js
@@ -1,14 +1,42 @@
+import { useEffect, useRef } from "react";
 import styles from "./MovieCards.module.scss";
 import PropTypes from "prop-types";
 import MovieCard from "../MovieCard";
-import MovieGroupingHeader from "../MovieGroupingHeader";
 
 const MovieCards = ({ moviesToDisplay, styleInfo }) => {
+  const moviesContainerEl = useRef(null);
+
+  useEffect(() => {
+    if (moviesToDisplay && moviesContainerEl && moviesContainerEl.current) {
+      const resizeObserver = new ResizeObserver((entries) => {
+        for (const entry of entries) {
+          // contentBoxSize implementation may vary across browsers. Usually it's an array, but it could be a single content rect
+          const contentBoxSize = Array.isArray(entry.contentBoxSize)
+            ? entry.contentBoxSize[0]
+            : entry.contentBoxSize;
+
+          if (
+            moviesContainerEl &&
+            moviesContainerEl.current &&
+            contentBoxSize.inlineSize < 465
+          ) {
+            moviesContainerEl.current.style.justifyContent = "center";
+          }
+        }
+      });
+      resizeObserver.observe(moviesContainerEl.current);
+    }
+  });
+
   return (
     <>
       {moviesToDisplay && (
         <>
-          <div className={styles["movies-container"]} style={styleInfo}>
+          <div
+            ref={moviesContainerEl}
+            className={styles["movies-container"]}
+            style={styleInfo}
+          >
             {moviesToDisplay.map((movie) => {
               return <MovieCard key={movie.id} {...movie} />;
             })}
