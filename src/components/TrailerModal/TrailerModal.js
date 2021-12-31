1b27e0d6afbac4417227fa205792213653bb5ec8
atorcode <atorcode@gmail.com>
2021-12-30 19:13:35 -0800

    Allow closing of TrailerModal with Escape key

--- src/components/TrailerModal/TrailerModal.js
+++ src/components/TrailerModal/TrailerModal.js
@@ -1,10 +1,23 @@
+import { useEffect } from "react";
 import styles from "./TrailerModal.module.scss";
 import { useSelectedMovieContext } from "../../contexts/SelectedMovieContext";
 
 const TrailerModal = () => {
   const { trailer, setVideoIsOpen } = useSelectedMovieContext();
 
-  console.log(trailer);
+  useEffect(() => {
+    const closeModal = (e) => {
+      if (e.key === "Escape") {
+        setVideoIsOpen(false);
+      }
+    };
+    document.addEventListener("keydown", closeModal);
+
+    return () => {
+      document.removeEventListener("keydown", closeModal);
+    };
+  }, []);
+
   return (
     <div
       className={styles["video-background"]}
